2d3af4a1dbeb34e9eb721897c7b197b21a12d9b1
Kristian Gonzalez <krisnrg@gmail.com>
2021-07-12 12:52:07 -0700

    Create dashboard.sh

---
+++ dashboard.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+function main(){
+  search="$(awk -F: '/run/&& NR>5{print $2}' "$0" | fzf )"
+  cmd="$(awk -v s="$search" '$0~s {getline; print $2}' "$0" | cut -d\( -f1 )"
+  $cmd
+  exit 0
+}
+
+# run:Git Stage
+function git_stage() {
+  echo "Staging files"
+}
+
+# run:Git Reset
+function git_reset() {
+    echo "Reseting files..."
+}
+
+# run:Downsync Live
+function downsync_live(){
+  echo "Getting live db..."
+}
+
+# run:Downsync Staging
+function downsync_live(){
+  echo "Getting live db..."
+}
+
+# run:Backup
+function backup() {
+    echo "Starting database backup..."
+}
+
+# run:Commit
+function commit() {
+    echo "Starting SVN commit..."
+}
+
+# run:Deploy Live
+function deploy_live() {
+    echo "Starting deploy to live..."
+}
+
+# run:Deploy Staging
+function deploy_staging() {
+    echo "Starting deploy to staging..."
+}
